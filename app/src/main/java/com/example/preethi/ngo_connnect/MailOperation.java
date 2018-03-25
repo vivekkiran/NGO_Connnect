8f8b26c735c46c8de76178952b0b191e614253bb
MahathiVavilala <mahathivavilala97@gmail.com>
2018-03-26 00:43:32 +0530

    Added MailOperation class

---
+++ app/src/main/java/com/example/preethi/ngo_connnect/MailOperation.java
@@ -0,0 +1,33 @@
+package com.example.preethi.ngo_connnect;
+
+/**
+ * Created by mahathi on 26/3/18.
+ */
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class MailOperation extends AsyncTask<Void, Void, String> {
+    @Override
+    protected String doInBackground(Void... params) {
+        try {
+            GMailSender sender = new GMailSender("mahathivavilala97@gmail.com", "Suz@nneWright17");
+            sender.sendMail("This is a testing mail",
+                    "You have successfully joined the event", "mahathivavilala97@gmail.com",
+                    "mahathivavilala97@gmail.com, anualek123@gmail.com");
+        } catch (Exception e) {
+            Log.e("error", e.getMessage(), e);
+            return "Email Not Sent";
+        }
+        return "Email Sent";
+    }
+    @Override
+    protected void onPostExecute(String result) {
+        Log.e("LongOperation", result + "");
+    }
+    @Override
+    protected void onPreExecute() {
+    }
+    @Override
+    protected void onProgressUpdate(Void... values) {
+    }
+}
